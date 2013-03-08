a59da0052876c6ca0675373e1f85a90ca024ee0b
Greg Tedder <gtedder@abriggs.com>
2013-03-08 00:17:36 -0600

    Unit Testing - Added a sample php unit test.

---
+++ mfw/tests/test.php
@@ -0,0 +1,14 @@
+<?php
+require '../config/config.php';
+
+class TEST extends PHPUnit_Framework_TestCase {
+	
+	public function testExample() {
+		$this->assertTrue(true);
+		$this->assertTrue(1);
+		$this->assertTrue(0);
+		$this->assertTrue(false);
+	}
+	
+}
+?>
